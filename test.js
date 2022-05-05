06b3a6bca88a6b1aeaef0e57331cd1bb57ae4923
Ray <ll9907891@gmail.com>
2022-05-05 14:46:10 +0800

    add rwd title

--- test.js
+++ test.js
@@ -7,18 +7,6 @@ $(document).ready(function () {
       this.$ele = $(ele);
       this.option = options;
     };
-    Module.DEFAULTS = {
-      count: {
-        // M版時每次點擊往前往後移動幾格儲存格
-        slide: 1, // [number]
-        // M版時一個畫面show幾格儲存格
-        show: 2, // [number]
-      },
-      // 設定花多久時間移動完成
-      speed: 0.3, // [number]
-      // 每次點擊儲存格時會執行此callback，並帶入所點擊的儲存格jquery物件
-      whenClick: "1",
-    };
 
     Module.prototype.test = function (opt) {
       if ($(window).width() <= 980) {
