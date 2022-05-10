05ba0c4caf0437fd406effc3ebcd9164bf871902
Ray <ll9907891@gmail.com>
2022-05-10 09:08:46 +0800

    update module func

--- index.js
+++ index.js
@@ -1,102 +1,15 @@
 $(document).ready(function () {
-  var waitForFinalEvent = (function () {
-    var timers = {};
-    return function (callback, ms, uniqueId) {
-      if (!uniqueId) {
-        uniqueId = "Don't call this twice without a uniqueId";
-      }
-      if (timers[uniqueId]) {
-        clearTimeout(timers[uniqueId]);
-      }
-      timers[uniqueId] = setTimeout(callback, ms);
-    };
-  })();
-  let callFunc = function () {
-    $(".frzTable.default").frzTable({
-      count: {
-        slide: 1, // [number]
+  $(".frzTable.default").frzTable({
+    count: {
+      slide: 1, // [number]
 
-        show: 2, // [number]
-      },
+      show: 2, // [number]
+    },
 
-      speed: 0.4, // [number]
+    speed: 0.3, // [number]
 
-      whenClick: function ($element) {
-        $element.addClass("whenclick");
-      },
-    });
-  };
-  callFunc();
-
-  $(window).resize(function () {
-    waitForFinalEvent(
-      function () {
-        callFunc();
-        // if ($(window).width() <= 980) {
-        //   $(".tbody-container-right").animate({ scrollLeft: "0" }, "slow");
-        //   let txt = $(".tbody-container-right").outerWidth();
-        //   let minwidth = Math.floor(txt / 2);
-        //   let slideMany = minwidth * 2;
-        //   let counter = minwidth * 2;
-        //   let slideWidth = minwidth * 7;
-        //   let arrowMargin = txt - 15;
-        //   if (counter === minwidth * 2) {
-        //     $("#previous-column").css("display", "none");
-        //   }
-        //   $("#next-column").css("display", "flex");
-        //   $(".date").css("min-width", `${minwidth}px`);
-        //   $(".sliding-window").css("width", `${slideWidth}px`);
-        //   $("#next-column").css("margin-left", `${arrowMargin}px`);
-        //   console.log("txt:", txt);
-        //   console.log("counter:", counter);
-        //   console.log("minwidth:", minwidth);
-        //   $("#next-column")
-        //     .unbind("click")
-        //     .click(function (event) {
-        //       event.preventDefault();
-
-        //       counter += minwidth;
-        //       console.log("+", counter);
-        //       if (counter === minwidth * 7) {
-        //         $("#next-column").css("display", "none");
-        //       }
-        //       if (counter !== minwidth * 2) {
-        //         $("#previous-column").css("display", "flex");
-        //       }
-        //       $(".tbody-container-right").animate(
-        //         { scrollLeft: `+=${minwidth}` },
-        //         "slow"
-        //       );
-        //     });
-        //   $("#previous-column")
-        //     .unbind("click")
-        //     .click(function (event) {
-        //       event.preventDefault();
-        //       counter -= minwidth;
-        //       console.log("counter:", counter);
-        //       console.log("mindth", minwidth * 2);
-        //       if (counter !== minwidth * 7) {
-        //         $("#next-column").css("display", "flex");
-        //       }
-        //       if (counter == minwidth * 2) {
-        //         $("#previous-column").css("display", "none");
-        //       }
-        //       $(".tbody-container-right").animate(
-        //         { scrollLeft: `-=${minwidth}` },
-        //         "slow"
-        //       );
-        //     });
-        // } else {
-        //   $(".tbody-container-right").animate({ scrollLeft: "0" }, "slow");
-        //   let txt = $(".tbody-container-right").outerWidth();
-        //   let minwidth = Math.floor(txt / 7);
-        //   $(".date").css("min-width", `${minwidth}px`);
-        //   $("#next-column").css("display", "none");
-        //   $("#previous-column").css("display", "none");
-        // }
-      },
-      500,
-      "some unique string"
-    );
+    whenClick: function ($element) {
+      $element.addClass("whenclick");
+    },
   });
 });
--- test.js
+++ test.js
@@ -16,7 +16,6 @@ $(document).ready(function () {
         let slideMany = Math.round(minwidth * `${opt.count.slide}`);
         let counter = minwidth * `${opt.count.show}`;
         let speed = opt.speed * 1000;
-
         let slideWidth = Math.round(minwidth * 7);
         let arrowMargin = txt - 15;
         $("#previous-column").css("display", "none");
@@ -60,7 +59,7 @@ $(document).ready(function () {
             );
           });
       } else {
-        $(".tbody-container-right").animate({ scrollLeft: "0" }, "slow");
+        // $(".tbody-container-right").animate({ scrollLeft: "0" }, "slow");
         let txt = $(".tbody-container-right").outerWidth();
         let minwidth = txt / 7;
         $(".date").css("min-width", `${minwidth}px`);
@@ -77,23 +76,36 @@ $(document).ready(function () {
         opt.whenClick($this);
       });
     };
+    Module.prototype.reSize = function (module, opt) {
+      let waitForFinalEvent = (function () {
+        var timers = {};
+        return function (callback, ms, uniqueId) {
+          if (!uniqueId) {
+            uniqueId = "Don't call this twice without a uniqueId";
+          }
+          if (timers[uniqueId]) {
+            clearTimeout(timers[uniqueId]);
+          }
+          timers[uniqueId] = setTimeout(callback, ms);
+        };
+      })();
+      $(window).resize(function () {
+        waitForFinalEvent(
+          function () {
+            module.test(opt);
+          },
+          500,
+          "some unique string"
+        );
+      });
+    };
 
     $.fn[ModuleName] = function (method, options) {
       return this.each(function () {
         let module = new Module(this, method);
-        let obj = null;
-        if (method.count.show > 5) {
-          obj = $.extend(true, method, {
-            count: {
-              slide: 1,
-              show: 5,
-            },
-          });
-        } else {
-          obj = $.extend({}, method);
-        }
-        module.test(obj);
+        module.test(method);
         module.testClick(method);
+        module.reSize(module, method);
       });
     };
   })(jQuery);
